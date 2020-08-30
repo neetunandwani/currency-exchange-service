f8748b7dbc8312aaf31278f797f4a4e9295414a5
Neetu Nandwani <neenandw@publicisgroupe.net>
2020-08-30 17:15:56 +0530

    logback

--- src/main/java/com/project/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/project/currencyexchangeservice/CurrencyExchangeController.java
@@ -5,9 +5,14 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import sun.rmi.runtime.Log;
+
+import java.util.logging.Logger;
 
 @RestController
 public class CurrencyExchangeController {
+
+    Logger logger = Logger.getLogger(CurrencyExchangeController.class.getName());
 
     @Autowired
     private CurrencyExchangeService service;
@@ -17,6 +22,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency/rate/from/{from}/to/{to}")
     public CurrencyExchange getRate(@PathVariable("from")String from, @PathVariable("to") String to){
+        logger.info("check sleuth exchange service");
         CurrencyExchange currencyExchange= service.find(from,to);
         currencyExchange.setPort(environment.getProperty("local.server.port"));
         return currencyExchange;
